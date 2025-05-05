53afec12473f2ab2c9de07e290a607de4691ccc6
Loladvr <loladvr4@gmail.com>
2025-05-05 19:35:04 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,1 @@
-function foncRouge() {
-	document.querySelector("#monPara").classList.remove("vert");
-	document.querySelector("#monPara").classList.add("rouge");
-}
-function foncVert() {
-	document.querySelector("#monPara").classList.remove("rouge");
-	document.querySelector("#monPara").classList.add("vert");
-}
+
